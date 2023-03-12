b58c393b8ca75c8aefdc5cfe670a8a87714f8729
RichardoTS <ri.tapiasaez@gmail.com>
2023-03-12 03:09:31 +0000

    Init Project

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("If you are reading this.. well hello!");
 };
+
+let inpt = document.getElementById("inpt");
+let btnDraw = document.getElementById("btnDraw");
+let btnSort = document.getElementById("btnSort");
+let crdContainer = document.getElementById("crdContainer");
+let crdLog = document.getElementById("crdLog");
+
+function getRandomInt() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber == 1) {
+    return "A";
+  } else if (randomNumber == 11) {
+    return "J";
+  } else if (randomNumber == 12) {
+    return "Q";
+  } else if (randomNumber == 13) {
+    return "K";
+  }
+  return randomNumber;
+}
+
+crdLog.innerHTML = getRandomInt();
+crdLog.appendChild;
