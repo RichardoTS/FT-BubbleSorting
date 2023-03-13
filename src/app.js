9d88821638af675098b52c5b54a960f5e0dda8f2
RichardoTS <ri.tapiasaez@gmail.com>
2023-03-13 01:53:03 +0000

    Project in progress

--- src/app.js
+++ src/app.js
@@ -5,68 +5,122 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let inpt = document.getElementById("inpt");
+let btnDraw = document.getElementById("btnDraw");
+let btnSort = document.getElementById("btnSort");
+let btnClear = document.getElementById("btnClear");
+let crdContainer = document.getElementById("crdContainer");
+let crdLog = document.getElementById("crdLog");
+
+let cards = [];
+
 window.onload = function() {
-  btnDraw.addEventListener("click", () => {
+  btnClear.addEventListener("click", () => {
     crdContainer.innerHTML = "";
     crdLog.innerHTML = "";
-    getRandomInt(inpt.value);
   });
 
-  btnSort.addEventListener("click", () => {
-    crdLog.innerHTML = "";
+  btnDraw.addEventListener("click", () => {
+    const numCards = parseInt(inpt.value);
+
+    for (let i = 0; i < numCards; i++) {
+      let cntnr = document.createElement("div");
+      let suit = document.createElement("div");
+      let suit2 = document.createElement("div");
+      let numb = document.createElement("div");
+
+      cntnr.classList = "crdContainer d-flex justify-content-around shadow";
+      numb.classList = "d-flex align-items-center";
+      suit.classList = "uppSuit";
+      suit2.classList = "btmSuit";
+
+      let result = getSuits();
+      suit.innerHTML = result;
+      suit2.innerHTML = result;
+      numb.innerHTML = getRandomInt();
+
+      // cards.push();
+
+      cntnr.appendChild(suit);
+      cntnr.appendChild(numb);
+      cntnr.appendChild(suit2);
+      console.log(cntnr);
+
+      crdContainer.appendChild(cntnr);
+    }
   });
+
+  // btnDraw = () => {
+  //   btnDraw(inpt.value);
+  // };
+
+  btnSort.addEventListener("click", () => {});
+
   //write your code here
   console.log("If you are reading this.. well hello!");
 };
 
-let inpt = document.getElementById("inpt");
-let btnDraw = document.getElementById("btnDraw");
-let btnSort = document.getElementById("btnSort");
-let crdContainer = document.getElementById("crdContainer");
-let uppSuit = document.getElementById("uppSuit");
-let mdlNum = document.getElementById("mdlNum");
-let btmSuit = document.getElementById("btmSuit");
-let crdLog = document.getElementById("crdLog");
-
-function deckBuilder() {
-  function getRandomInt() {
-    let randomNumber = Math.floor(Math.random() * 13) + 1;
-    if (randomNumber == 1) {
-      return "A";
-    } else if (randomNumber == 11) {
-      return "J";
-    } else if (randomNumber == 12) {
-      return "Q";
-    } else if (randomNumber == 13) {
-      return "K";
-    }
-    return randomNumber;
+// function deckBuilder() {
+// }
+function getRandomInt() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber == 1) {
+    return "A";
+  } else if (randomNumber == 11) {
+    return "J";
+  } else if (randomNumber == 12) {
+    return "Q";
+  } else if (randomNumber == 13) {
+    return "K";
   }
+  return randomNumber;
+}
 
-  function getSuit(suitNumber = 0) {
-    switch (suitNumber) {
-      case 1:
-        return "♦";
-      case 2:
-        return "♥";
-      case 3:
-        return "♠";
-      case 4:
-        return "♣";
-    }
-  }
-  function getSuits() {
-    let theSuits = getSuit(Math.floor(Math.random() * 4) + 1);
-    return theSuits;
+function getSuit(suitNumber = 0) {
+  switch (suitNumber) {
+    case 1:
+      return "♦";
+    case 2:
+      return "♥";
+    case 3:
+      return "♠";
+    case 4:
+      return "♣";
   }
 }
-//<--- eliminar comentario let result = getSuits();
+function getSuits() {
+  let theSuits = getSuit(Math.floor(Math.random() * 4) + 1);
+  return theSuits;
+}
+// if (result == "♦" || result == "♥") {
+//   upperSuit.style.color = "red";
+//   bottmSuit.style.color = "red";
+//   middleNum.style.color = "red";
+// } else {
+//   upperSuit.style.color = "black";
+//   bottmSuit.style.color = "black";
+//   middleNum.style.color = "black";
+// }
 
-uppSuit.innerHTML = result;
-mdlNum.innerHTML = getRandomInt();
-btmSuit.innerHTML = result;
-uppSuit.appendChild = result;
-mdlNum.appendChild = getRandomInt();
-btmSuit.appendChild = result;
+let result = getSuits();
+let int = getRandomInt();
 
-console.log(getSuits());
+console.log("The suit is: " + result + ", and the number is: " + int);
+
+// const bubbleSort = arr => {
+//   let wall = arr.length - 1; //we start the wall at the end of the array
+//   while (wall > 0) {
+//     let index = 0;
+//     while (index < wall) {
+//       //compare the adjacent positions, if the right one is bigger, we have to swap
+//       if (arr[index] > arr[index + 1]) {
+//         let aux = arr[index];
+//         arr[index] = arr[index + 1];
+//         arr[index + 1] = aux;
+//       }
+//       index++;
+//     }
+//     wall--; //decrease the wall for optimization
+//   }
+//   return arr;
+// };
